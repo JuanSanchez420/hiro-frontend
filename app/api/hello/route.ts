bc030e316a25e004c5200a8b8b61b2135686dde0
JuanSanchez420 <juansanchez420@protonmail.com>
2025-02-10 23:28:33 -0600

    test route

---
+++ app/api/hello/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+
+    const apiUrl = `${process.env.NEXT_PUBLIC_EXPRESS_URL}:${process.env.NEXT_PUBLIC_EXPRESS_PORT}/api/hello`;
+
+    // Forward the request to the server
+    const response = await fetch(apiUrl, {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        cookie: req.headers.get('cookie') || '',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined,
+    });
+
+    const headers = new Headers(response.headers);
+
+    const data = await response.json();
+    return NextResponse.json(data, {
+      status: response.status,
+      headers: headers
+    });
+  } catch (error) {
+    console.error('Error connecting to server:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
