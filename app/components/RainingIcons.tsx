637e04195d8669f0899abe90141fdae1e4b2cef9
JuanSanchez420 <juansanchez420@protonmail.com>
2025-02-13 13:48:55 -0600

    compile fix

--- app/components/RainingIcons.tsx
+++ app/components/RainingIcons.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import React, { useEffect, useMemo } from "react";
-import { useMessagesContext } from "../context/MessagesContext";
 import tokens from "../utils/tokens.json";
+import { useGlobalContext } from "../context/GlobalContext";
 
-const FallingIcons: React.FC = () => {
+const RainingIcon: React.FC = () => {
   const [loaded, setLoaded] = React.useState(false);
-  const { rain, setRain } = useMessagesContext();
+  const { rain, setRain } = useGlobalContext();
   const count = 30;
 
   const token = tokens[rain as keyof typeof tokens];
@@ -16,19 +16,19 @@ const FallingIcons: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if(rain) setTimeout(() => setRain(undefined), 10_000);
-  },[rain, setRain])
+    if (rain) setTimeout(() => setRain(undefined), 10_000);
+  }, [rain, setRain])
 
-  const icons = useMemo(()=>Array.from({ length: count }, (_, i) => {
+  const icons = useMemo(() => Array.from({ length: count }, (_, i) => {
     const leftPosition = Math.random() * 80;
     return (
       <div key={i} className="falling-icon" style={{ left: `${leftPosition}vw` }}>
         <img src={token?.logoURI} className="size-8" alt="" />
       </div>
     );
-  }),[ token?.logoURI, count]);
+  }), [token?.logoURI, count]);
 
   return loaded && token?.logoURI ? <div>{icons}</div> : null;
 }
 
-export default React.memo(FallingIcons);
+export default React.memo(RainingIcon);
